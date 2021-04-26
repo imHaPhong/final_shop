126c1a292cfd9fd64b8bafa08ad5900e651a2ff6
imHaPhong <64822110+imHaPhong@users.noreply.github.com>
2021-04-26 21:47:45 +0700

    them tinh nang cho nha hang

--- src/config/socket.js
+++ src/config/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
-// export const socket = io("https://tuanna-final.herokuapp.com/", {
-//   transports: ["websocket"],
-// });
-export const socket = io("http://localhost:8080/", {
+export const socket = io("https://tuanna-final.herokuapp.com/", {
   transports: ["websocket"],
 });
+// export const socket = io("http://localhost:8080/", {
+//   transports: ["websocket"],
+// });
